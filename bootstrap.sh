741f7b88cada2071b3a17329ddc27f41a56c5190
Joe Wilner <jhwilner1@gmail.com>
2020-10-03 18:47:31 -0400

    Also handle licensor

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
 readonly CUR_PATH="github.com/jwilner/go-tmpl"
+readonly CUR_LICENSOR="Joe Wilner"
 
 main() {
-    if [[ -z "${1}" ]]; then
+    if [[ -z "${1}" ]] || [[ -z "${2}" ]]; then
         cat <<EOF >&2
-USAGE: bootstrap.sh PACKAGE_PATH" >&2
+USAGE: bootstrap.sh PACKAGE_PATH LICENSOR"
 
-PACKAGE_PATH is the path as used for go modules; e.g. github.com/jwilner/go-tmpl
+- PACKAGE_PATH is the path as used for go modules; e.g. "${CUR_PATH}".
+- LICENSOR is usually the author or owning organization; e.g. "${CUR_LICENSOR}".
 EOF
         exit 1
     fi
 
-    LC_ALL=C find . -path ./.git -prune -false -o -type f -exec sed -i.bak 's!'"${CUR_PATH}"'!'"${1}"'!g' {} \;
+    LC_ALL=C find . -path ./.git -prune -false -o -type f -exec sed -i.bak '
+s!'"${CUR_PATH}"'!'"${1}"'!g;
+s!'"${CUR_LICENSOR}"'!'"${2}"'!g' {} \;
+
     find . -path ./.git -prune -false -o -type f -name '*.bak' -delete
 
     rm bootstrap.sh
